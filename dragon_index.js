ed440d56d3b1cea571f0c2a014043798eddfc007
Marisha Parker <m_parker2@wargaming.net>
2019-10-29 08:49:47 -0700

    Fixed translate function and some other matrix calcs, but rotation still not occurring as expected. suspect need to transpose x and y variables

--- dragon_index.js
+++ dragon_index.js
@@ -2,6 +2,8 @@ let canvas;
 let dragon = [];
 let UNIT_LENGTH = 10;
 const ANIM_RATE = 1000;
+const R = [[0, -1], [1, 0]];
+
 
 let animLoop;
 
@@ -21,7 +23,6 @@ function initCanvas() {
 function initDragon() {
 	dragon.push([canvas.center.x, canvas.center.y]);
 	dragon.push([canvas.center.x, canvas.center.y + UNIT_LENGTH]);
-	// canvas.drawLineFrom(dragon[0], dragon[1]);
 }
 
 function initAnimation() {
@@ -31,11 +32,9 @@ function initAnimation() {
 function animateDragon() {
 	canvas.context.clearRect(0, 0, canvas.width, canvas.height);
 	for (let i = 0; i < dragon.length - 1; i += 2) {
-		// let j = i;
-		// canvas.drawLineFrom(dragon[j], dragon[j + 1], '#FFFFFF');
 		canvas.drawLineFrom(dragon[i], dragon[i + 1], '#FFFFFF');
 	}
-	// iterateDragon();
+	iterateDragon();
 }
 
 function iterateDragon() {
@@ -52,66 +51,55 @@ function iterateDragon() {
  * @param modifier [x, y]
  */
 function translate(vector, modifier) {
-	vector.forEach(coord => {
-		coord[0] += modifier[0]
-		coord[1] += modifier[1]
-	});
-	return vector;
+	return vector.map(coord => [
+		coord[0] + modifier[0],
+		coord[1] + modifier[1]
+	]);
 }
 
 function rotateClockwise(vector, angle=90) {
 	// TODO: make vector diagonal?
-	const R = rotationMatrix(angle);
-	console.log('Value of R with angle', angle, vector);
+	const offset = [-vector[0][0], -vector[0][1]];
+
 
 	for (let i = 0; i < vector.length - 1; i += 2) {
 		// Move to center
-		const offset = [-vector[i][0], -vector[i][1]];
-		let v1 = translate(vector[i], offset);
-		let v2 = translate(vector[i + 1], offset);
+		let v = translate([vector[i], vector[i + 1]], offset);
 
 		// Rotate around origin
-		const product = dotProduct(R, v1.concat(v2));
+		const product = dotProduct(R, v);
 
 		// Move back to location
-		v1 = translate(product.slice(0, 2), [-offset[0], -offset[1]]);
-		v2 = translate(product.slice(2), [-offset[0], -offset[1]]);
+		v = translate(product, [-offset[0], -offset[1]]);
 
 		// Reassign value
-		vector[i] = v1;
-		vector[i + 1] = v2;
+		vector[i] = v[0];
+		vector[i + 1] = v[1];
 	}
 	return vector;
 }
 
-const rotationMatrix = (angle) => [
-	Math.cos(angle), Math.sin(angle),
-	-1 * Math.sin(angle), Math.cos(angle)
-];
+
 
 // [ x1, y1 ]
 // [ x2, y2 ]
-// [ x3, y3 ]
-// [ x4, y4 ]
 // NOTE: This function only works for two-column source, two-row target.
 function dotProduct(source, target, rows) {
-	console.log('source:', source);
-	console.log('target:', target);
-
 	if (source.length !== target.length) {
 		console.error("Cannot perform dot product of unequal count", source, target);
 		throw new Error ("Cannot perform dot product of unequal count");
 	}
 
-	const out = [];
+	const v1 = [];
+	const v2 = [];
 
-	out.push((source[0] * target[0]) + (source[1] * target[2]));
-	out.push((source[0] * target[1]) + (source[1] * target[3]))
+	v1.push((source[0][0] * target[0][0]) + (source[0][1] * target[1][0]));
+	v1.push((source[0][0] * target[0][1]) + (source[0][1] * target[1][1]))
 
-	out.push((source[2] * target[0]) + (source[3] * target[2]));
-	out.push((source[2] * target[1]) + (source[3] * target[3]))
-
-	return out;
+	v2.push((source[1][0] * target[0][0]) + (source[1][1] * target[1][0]));
+	v2.push((source[1][0] * target[0][1]) + (source[1][1] * target[1][1]))
+	
+	return [v1, v2];
 }
 
 function stopGrowing() {
